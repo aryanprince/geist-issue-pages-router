b5a83d6d694a3488ac7e098d7038e2e38b9da0c5
Aryan <aryanprincesanty@icloud.com>
2023-10-31 04:54:00 +0000

    Error happens here :(

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+
+import { GeistSans } from "geist/font";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={GeistSans.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
