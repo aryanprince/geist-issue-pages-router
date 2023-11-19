4ed0be9071487147833409463b92b34249f2083c
Aryan Prince <aryanprincesanty@icloud.com>
2023-11-19 03:03:26 +0000

    Updated home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,12 @@
 export default function Home() {
   return (
-    <div>Hello World</div>
+    <>
+      <h1 className="text-4xl font-semibold tracking-tight">
+        this text uses geist font, errors errors :(
+      </h1>
+      <p className="text-2xl font-semibold text-neutral-400">
+        ver. geist@1.1.0
+      </p>
+    </>
   )
 }
